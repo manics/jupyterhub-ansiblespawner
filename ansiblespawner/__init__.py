3f59c6aff371529d5ae24228230c7ab8d5fd2fcf
Simon Li <orpheus+devel@gmail.com>
2020-06-26 16:57:23 +0100

    Add AnsibleException to __init__.py

--- ansiblespawner/__init__.py
+++ ansiblespawner/__init__.py
@@ -1,4 +1,4 @@
 from ._version import version as __version__
-from .ansiblespawner import AnsibleSpawner
+from .ansiblespawner import AnsibleSpawner, AnsibleException
 
-__all__ = ["__version__", "AnsibleSpawner"]
+__all__ = ["__version__", "AnsibleSpawner", "AnsibleException"]
