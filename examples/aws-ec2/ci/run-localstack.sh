6c354b3c2b37f4199ed324c5d1abc48ebbac53f2
Simon Li <orpheus+devel@gmail.com>
2021-11-14 18:10:28 +0000

    localstack/localstack:0.12.20

--- examples/aws-ec2/ci/run-localstack.sh
+++ examples/aws-ec2/ci/run-localstack.sh
@@ -10,7 +10,10 @@ else
   CONTAINER=docker
 fi
 
+#$CONTAINER run --rm --name localstack -p 4566:4566 \
+#  -e SERVICES=ec2,iam,sts \
+#  -v ./patch-entrypoint.sh:/usr/local/bin/patch-entrypoint.sh:ro,z \
+#  --entrypoint patch-entrypoint.sh "$@" localstack/localstack:0.12.20
 $CONTAINER run --rm --name localstack -p 4566:4566 \
   -e SERVICES=ec2,iam,sts \
-  -v ./patch-entrypoint.sh:/usr/local/bin/patch-entrypoint.sh:ro,z \
-  --entrypoint patch-entrypoint.sh "$@" localstack/localstack:0.12.16
+  localstack/localstack:0.12.20
