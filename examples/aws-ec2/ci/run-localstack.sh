6a41e037bbb19cd24312f1abebcd91220da71f45
Simon Li <orpheus+devel@gmail.com>
2024-02-18 23:28:23 +0000

    localstack/localstack:3.1

--- examples/aws-ec2/ci/run-localstack.sh
+++ examples/aws-ec2/ci/run-localstack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+LOCALSTACK_VERSION=3.1
+
 SCRIPTDIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 cd "$SCRIPTDIR"
 
@@ -10,10 +12,7 @@ else
   CONTAINER=docker
 fi
 
-#$CONTAINER run --rm --name localstack -p 4566:4566 \
-#  -e SERVICES=ec2,iam,sts \
-#  -v ./patch-entrypoint.sh:/usr/local/bin/patch-entrypoint.sh:ro,z \
-#  --entrypoint patch-entrypoint.sh "$@" localstack/localstack:0.12.20
 $CONTAINER run --rm --name localstack -p 4566:4566 \
   -e SERVICES=ec2,iam,sts \
-  localstack/localstack:0.12.20
+  -v ./patch-entrypoint.sh:/usr/local/bin/patch-entrypoint.sh:ro,z \
+  --entrypoint patch-entrypoint.sh "$@" localstack/localstack:$LOCALSTACK_VERSION
